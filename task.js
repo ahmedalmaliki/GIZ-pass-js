b695b23d7787f5a4c17df5b93a2d7eb1e661ecaf
ahmedalmaliki <ahmedalmalikiab@gmail.com>
2021-09-30 14:35:38 +0300

    Almost ready

--- task.js
+++ task.js
@@ -1,4 +1,4 @@
-let numbers = [
+const numbers = [
     5,
     8,
     0,
@@ -11,34 +11,71 @@ let numbers = [
 
 console.log("Original numbers list: ", numbers)
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
+//To keep the original array immutable we have to create a new one and copy the items of the original array to it.
+
+const sortedNumbers = [...numbers];
+
+console.log("copied numbers list: ", sortedNumbers)
+
+//Now we can mutate the sortedNumbers list while keeping the original one intact.
+
+let shift; //This variable helps us stop excuting if the array is already sorted.
+
+
+function AscendBubbleSort(unsortedNumbers){
+	  shift = false;
+	  let endOfArray = unsortedNumbers.length - 1; //This variable stops the loop before the last item, since the code is always looking at the item on the left.
+	  
+	  for (let i = 0; i < endOfArray; i++){
+	  	    if(unsortedNumbers[i] > unsortedNumbers[i+1]){
+	  	    	  shift = true; 
+	  	    	  let temp = unsortedNumbers[i];
+	  	    	  unsortedNumbers[i] = unsortedNumbers[i+1];
+	  	    	  unsortedNumbers[i+1] = temp;
+	  	    	  
+	  	    }
+	  }
+	  endOfArray --;
 }
 
-console.log("Numbers list After sorting: ", numbers)
+do {
+	 AscendBubbleSort(sortedNumbers);
+}while(shift);
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
+
+console.log("Numbers list After Ascend sorting: ", sortedNumbers)
+
+
+
+function DescendBubbleSort(unsortedNumbers){
+	  shift = false;
+	  let endOfArray = unsortedNumbers.length - 1;
+	  for (let i = 0; i < endOfArray; i++){
+	  	    if(unsortedNumbers[i] < unsortedNumbers[i+1]){
+	  	    	  shift = true; 
+	  	    	  let temp = unsortedNumbers[i];
+	  	    	  unsortedNumbers[i] = unsortedNumbers[i+1];
+	  	    	  unsortedNumbers[i+1] = temp;
+	  	    	  
+	  	    }
+	  }
+	  endOfArray --;
 }
 
-console.log("Numbers list After Desc sorting: ", numbers)
+
+do {
+	 DescendBubbleSort(sortedNumbers);
+}while(shift);
+
+
+console.log("Numbers list After Desc sorting: ", sortedNumbers)
+
+function readFileIntoArray(fileName){
+	 var fs = require('fs')
+	 var array = fs.readFileSync(fileName).toString().split(",");
+	 return array;
+}
+
+console.log(readFileIntoArray('data.txt'));
 
 
